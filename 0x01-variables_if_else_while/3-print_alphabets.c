1dae14fede793cd88a8baa7cb9821f06693b95f8
JINMORI21 <said.ghazzaf2002@gmaim.com>
2023-06-16 13:20:55 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main() {
+char lowercase = 'a';
+char uppercase = 'A';
+		while (lowercase <= 'z')
+{
+	putchar(lowercase++);
+}
+		while (uppercase <= 'Z')
+{
+	putchar(uppercase++);
+}
+	putchar('\n');
+return (0);
+}
