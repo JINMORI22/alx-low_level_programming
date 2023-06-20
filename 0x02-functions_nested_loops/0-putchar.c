e434d5b3cab845afd8ce3f279e4cd119fd6ee31b
JINMORI21 <said.ghazzaf2002@gmaim.com>
2023-06-20 04:16:19 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main() {
+char string[] = "_putchar\n";
+int i = 0;
+while (string[i] != '\0')
+{
+	putchar(string[i]);
+	i++;
+}
+return 0;
+}
