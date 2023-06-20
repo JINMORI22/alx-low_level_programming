28810ec306fcc87d21a35ec906d47f7081865a63
JINMORI21 <said.ghazzaf2002@gmaim.com>
2023-06-20 02:56:35 -0700

    small change

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-char lowercase = 'a';
-char uppercase = 'A';
-while (lowercase <= 'z')
+	char lowercase = 'a';
+	char uppercase = 'A';
+	while (lowercase <= 'z')
 {
 	putchar(lowercase++);
 }
-while (uppercase <= 'Z')
+	while (uppercase <= 'Z')
 {
 	putchar(uppercase++);
 }
 	putchar('\n');
-return (0);
+	return (0);
 }
