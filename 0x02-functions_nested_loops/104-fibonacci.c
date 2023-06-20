e3776a4cfcdf7d5d1a7e207442501c0ec9a93a52
JINMORI21 <said.ghazzaf2002@gmaim.com>
2023-06-20 05:06:25 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
-void fibonacci_sequence(int n)
-{
-unsigned long long fib_sequence[98];
-		fib_sequence[0] = 1;
-		fib_sequence[1] = 2;
-	for (int i = 2; i < n; i++)
-	{
-		fib_sequence[i] = fib_sequence[i-1] + fib_sequence[i-2];
-	}
-	for (int i = 0; i < n-1; i++)
-	{
-		printf("%llu, ", fib_sequence[i]);
-	}
-		printf("%llu\n", fib_sequence[n-1]);
-}
 int main(void)
 {
-	fibonacci_sequence(98);
-return (0);
+	int i;
+	unsigned int a, b, c;
+
+	a = 1;
+	b = 2;
+
+	printf("%u, %u, ", a, b);
+
+	for (i = 3; i <= 98; i++)
+	{
+		c = a + b;
+		printf("%u", c);
+
+		if (i < 98)
+		{
+			printf(", ");
+		}
+		a = b;
+		b = c;
+	}
+	printf("\n");
+
+	return (0);
 }
